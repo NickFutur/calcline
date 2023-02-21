7257d3821db0daf3452515dc23cd46669ec8cb19
Nick Morin <morinnick31@gmail.com>
2023-02-21 15:01:03 +0300

    change divison on zero on line 2

--- js/main.js
+++ js/main.js
@@ -19,19 +19,111 @@ document.addEventListener('input', function(event) {
         let L = Number(document.getElementById("L").value);
         let T8 = Number(document.getElementById("T8").value);
         let T9 = Number(document.getElementById("T9").value);
+        if (N1 != 10 && N2 != 0 && N3 != 0 && N4 != 0 && M != 0 && V != 0 && (T10 * 60 - T1 - T5 - T8 - T9) != 0) {
+            let HelpOperation = Number(((T2 / N2) * N4) + ((T3 / N3) * N4) + ((T4 / M) * N4) + ((T7 * G * L) / ((N1 - 10) * 1000 * 1000)) * N4).toFixed(4);
+            let T0 = Number(((T2 / N2) * N4) + ((T3 / N3) * N4) + ((T4 / M) * N4) + ((((T7 * G * L) / ((N1 - 10) * 1000 * 1000)) * N4) + T6 + (N4 / V))).toFixed(4);
+            let K = Number((T10 * 60) / (T10 * 60 - T1 - T5 - T8 - T9)).toFixed(4);
+            let T = Number(T0 * K).toFixed(4);
+            let P = Number((T10 * 60 - T1 - T5 - T8 - T9) / (T2 / N2 + T3 / N3 + T4 / M + T7 * G * L / (N1 - 10) / 1000000 + T6 / N4 + 1 / V)).toFixed(4);
+            document.getElementById("HelpOperation").textContent = HelpOperation;
+            document.getElementById("T0").innerHTML = T0;
+            document.getElementById("K").innerHTML = K;
+            document.getElementById("T").innerHTML = T;
+            document.getElementById("P").innerHTML = P;
+            console.log(Number(((T10 * 60) / T) * N4).toFixed(4));
+            // Расчёт исключений
+        } else if (N1 == 10 || N2 == 0 || N3 == 0 || N4 == 0 || M == 0 || V == 0 || (T10 * 60 - T1 - T5 - T8 - T9) == 0) {
+            function exclusionFun(e) {
+                let N1_num = Number((T7 * G * L) / ((N1 - 10) * 1000000));
+                let T2_N2 = Number(T2 / N2);
+                let T3_N3 = Number(T3 / N3);
+                let T6_N4 = Number(T6 / N4);
+                let T4_M = Number(T4 / M);
+                let N4_V = Number(N4 / V);
+                let V_1 = Number(1 / V);
+                let T10_nums = Number((T10 * 60) / (T10 * 60 - T1 - T5 - T8 - T9));
 
-        let HelpOperation = Number(((T2 / N2) * N4) + ((T3 / N3) * N4) + ((T4 / M) * N4) + ((T7 * G * L) / ((N1 - 10) * 1000 * 1000)) * N4).toFixed(4);
-        let T0 = Number(((T2 / N2) * N4) + ((T3 / N3) * N4) + ((T4 / M) * N4) + ((((T7 * G * L) / ((N1 - 10) * 1000 * 1000)) * N4) + T6 + (N4 / V))).toFixed(4);
-        let K = Number((T10 * 60) / (T10 * 60 - T1 - T5 - T8 - T9)).toFixed(4);
-        let T = Number(T0 * K).toFixed(4);
-        let P = Number((T10 * 60 - T1 - T5 - T8 - T9) / (T2 / N2 + T3 / N3 + T4 / M + T7 * G * L / (N1 - 10) / 1000000 + T6 / N4 + 1 / V)).toFixed(4);
-        document.getElementById("HelpOperation").textContent = HelpOperation;
-        document.getElementById("T0").innerHTML = T0;
-        document.getElementById("K").innerHTML = K;
-        document.getElementById("T").innerHTML = T;
-        document.getElementById("P").innerHTML = P;
-        console.log(Number(((T10 * 60) / T) * N4).toFixed(4));
-    } else {
+                if (!isFinite(N1_num) || isNaN(N1_num)) {
+                    N1_num = 0;
+                }
+                if (!isFinite(T2_N2) || isNaN(T2_N2)) {
+                    T2_N2 = 0;
+                }
+                if (!isFinite(T3_N3) || isNaN(T3_N3)) {
+                    T3_N3 = 0;
+                }
+                if (!isFinite(T6_N4) || isNaN(T6_N4)) {
+                    T6_N4 = 0;
+                }
+                if (!isFinite(T4_M) || isNaN(T4_M)) {
+                    T4_M = 0;
+                }
+                if (!isFinite(N4_V) || isNaN(N4_V)) {
+                    N4_V = 0;
+                }
+                if (!isFinite(V_1) || isNaN(V_1)) {
+                    V_1 = 0;
+                }
+                if (!isFinite(T10_nums) || isNaN(T10_nums)) {
+                    T10_nums = 0;
+                }
+                console.log('N1_num = ' + N1_num);
+                console.log('T2_N2 = ' + T2_N2);
+                console.log('T3_N3 = ' + T3_N3);
+                console.log('T6_N4 = ' + T6_N4);
+                console.log('T4_M = ' + T4_M);
+                console.log('V_1 = ' + V_1);
+                console.log('T10_nums = ' + T10_nums);
+                console.log(' --------------');
+                console.log(' ');
+                // выражение P_Answer не переносить вверх, потому что может нарушиться логика
+                let P_Answer = Number(T10_nums / (T2_N2 + T3_N3 + T4_M + N1_num + T6_N4 + V_1));
+                // Уравнения, результаты которых будут выводиться на сайте 
+                let HelpOperation = Number((T2_N2 * N4) + (T3_N3 * N4) + (T4_M * N4) + (N1_num * N4));
+                console.log('T2_N2 * N4 = ' + T2_N2 * N4);
+                console.log('T3_N3 * N4 = ' + T3_N3 * N4);
+                console.log('T4_M * N4 = ' + T4_M * N4);
+                console.log('N1_num = ' + N1_num);
+                console.log('N1_num * N4 = ' + N1_num * N4);
+                console.log(' +++++++++++++++ ');
+                console.log(' ');
+                console.log(' ');
+                let T0 = Number(HelpOperation + T6 + N4_V);
+                let K = T10_nums;
+                let T = Number(T0 * K);
+                // Проверка для P_1, если оно Infinity или NaN
+                if (!isFinite(P_Answer) || isNaN(P_Answer)) {
+                    P_Answer = 0;
+                }
+                let P1 = P_Answer;
+
+                // Вывод элементов на страницу
+                document.getElementById("HelpOperation").textContent = HelpOperation.toFixed(4);
+                document.getElementById("T0").innerHTML = T0.toFixed(4);
+                document.getElementById("K").innerHTML = K.toFixed(4);
+                document.getElementById("T").innerHTML = T.toFixed(4);
+                document.getElementById("P").innerHTML = P1.toFixed(4);
+            }
+            exclusionFun();
+        } else {
+            console.log('Непредвиденные ошибки!');
+            return;
+        }
+
+    }
+    //     let HelpOperation = Number(((T2 / N2) * N4) + ((T3 / N3) * N4) + ((T4 / M) * N4) + ((T7 * G * L) / ((N1 - 10) * 1000 * 1000)) * N4).toFixed(4);
+    //     let T0 = Number(((T2 / N2) * N4) + ((T3 / N3) * N4) + ((T4 / M) * N4) + ((((T7 * G * L) / ((N1 - 10) * 1000 * 1000)) * N4) + T6 + (N4 / V))).toFixed(4);
+    //     let K = Number((T10 * 60) / (T10 * 60 - T1 - T5 - T8 - T9)).toFixed(4);
+    //     let T = Number(T0 * K).toFixed(4);
+    //     let P = Number((T10 * 60 - T1 - T5 - T8 - T9) / (T2 / N2 + T3 / N3 + T4 / M + T7 * G * L / (N1 - 10) / 1000000 + T6 / N4 + 1 / V)).toFixed(4);
+    //     document.getElementById("HelpOperation").textContent = HelpOperation;
+    //     document.getElementById("T0").innerHTML = T0;
+    //     document.getElementById("K").innerHTML = K;
+    //     document.getElementById("T").innerHTML = T;
+    //     document.getElementById("P").innerHTML = P;
+    //     console.log(Number(((T10 * 60) / T) * N4).toFixed(4));
+    // } 
+    else {
         return;
     }
 });
